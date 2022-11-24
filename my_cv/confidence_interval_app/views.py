56a4d6689a57fbae804fcb02313383a0d4d9a766
Dmitry Korolev <dkorolev13@gmail.com>
2022-11-24 22:10:09 +0300

    add try/except for bad file format

--- my_cv/confidence_interval_app/views.py
+++ my_cv/confidence_interval_app/views.py
@@ -8,16 +8,21 @@ def index(request):
     if request.method == 'POST':
 
         if 'document' in request.FILES:
-
             uploaded_file = request.FILES['document']
+            try:
 
-            data = pd.read_csv(uploaded_file, sep='\t', header=None)
-            data_tmp = data[[36, 41, 42]]
-            mean = data_tmp[(data_tmp[41] == 'FollowingMode') & (data_tmp[42] == 'success')][36].mean()
-            std = data_tmp[(data_tmp[41] == 'FollowingMode') & (data_tmp[42] == 'success')][36].std()
-            ans = round(mean + 2 * std, 2)
-            content = {'data': ans,
-                       'counter': len(data_tmp[(data_tmp[41] == 'FollowingMode') & (data_tmp[42] == 'success')])}
+                data = pd.read_csv(uploaded_file, sep='\t', header=None)
+                data_tmp = data[[36, 41, 42]]
+                mean = data_tmp[(data_tmp[41] == 'FollowingMode') & (data_tmp[42] == 'success')][36].mean()
+                std = data_tmp[(data_tmp[41] == 'FollowingMode') & (data_tmp[42] == 'success')][36].std()
+                ans = round(mean + 2 * std, 2)
+                content = {'data': ans,
+                           'counter': len(data_tmp[(data_tmp[41] == 'FollowingMode') & (data_tmp[42] == 'success')])}
+
+            except pd.errors.ParserError:
+                content = {'data': 'Неверный формат log файла',
+                           'counter': 'Неверный формат log файла'}
+
         else:
             content = {'data': '...',
                        'counter': '...'}
