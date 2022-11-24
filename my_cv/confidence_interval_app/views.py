bdb9cead37e7b63efadad740238fca6544f48776
Dmitry Korolev <dkorolev13@gmail.com>
2022-11-24 14:42:18 +0300

    Убрал баг при отправке пустой формы

--- my_cv/confidence_interval_app/views.py
+++ my_cv/confidence_interval_app/views.py
@@ -3,21 +3,21 @@ import pandas as pd
 
 
 def index(request):
+    content = {'data': '...'}
     if request.method == 'POST':
-        uploaded_file = request.FILES['document']
 
-        data = pd.read_csv(uploaded_file, sep='\t', header=None)
-        data_tmp = data[[36, 41, 42]]
-        data_tmp[(data_tmp[41] == 'FollowingMode') & (data_tmp[42] == 'success')]
-        mean = data_tmp[(data_tmp[41] == 'FollowingMode') & (data_tmp[42] == 'success')][36].mean()
-        std = data_tmp[(data_tmp[41] == 'FollowingMode') & (data_tmp[42] == 'success')][36].std()
-        # data = data[[36, 41, 42]]
-        # data[(data[41] == 'FollowingMode') & (data[42] == 'success')]
-        # mean = data[(data[41] == 'FollowingMode') & (data[42] == 'success')][36].mean()
-        # std = data[(data[41] == 'FollowingMode') & (data[42] == 'success')][36].std()
+        if 'document' in request.FILES:
 
-        ans = mean + 2 * std
-    content = {'data': ans}
+            uploaded_file = request.FILES['document']
 
-    # print(data.head(3))
+            data = pd.read_csv(uploaded_file, sep='\t', header=None)
+            data_tmp = data[[36, 41, 42]]
+            mean = data_tmp[(data_tmp[41] == 'FollowingMode') & (data_tmp[42] == 'success')][36].mean()
+            std = data_tmp[(data_tmp[41] == 'FollowingMode') & (data_tmp[42] == 'success')][36].std()
+
+            ans = round(mean + 2 * std, 2)
+            content = {'data': ans}
+        else:
+            content = {'data': '...'}
+
     return render(request, 'confidence_interval_app/index.html', content)
